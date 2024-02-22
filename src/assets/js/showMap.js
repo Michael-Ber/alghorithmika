c7285544453bac9be4e41949050c044522ab76e4
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2024-02-22 09:55:37 +0800

    map modal done

--- src/assets/js/showMap.js
+++ src/assets/js/showMap.js
@@ -7,14 +7,18 @@ export const showMap = () => {
         map.classList.add('modal-map');
         const mapScript = document.createElement('div');
         mapScript.classList.add('modal-map__script');
-        mapScript.innerHTML = `<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa10fbb640e6ba5fd618bbc52b24833beac883e0e5036130f525f73a31502bd09&amp;source=constructor" width="748" height="400" frameborder="0"></iframe>`;
+        mapScript.innerHTML = `
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa10fbb640e6ba5fd618bbc52b24833beac883e0e5036130f525f73a31502bd09&amp;source=constructor" width="748" height="400" frameborder="0"></iframe>
+            <div class="modal-map__close" >&#10006;</div>
+        `;
 
         mapImg.addEventListener('click', () => {
             map.appendChild(mapScript);
             body.appendChild(map);
         })
+
         window.addEventListener('click', (e) => {
-            if (e.target.classList.contains('modal-map')) {
+            if (e.target.classList.contains('modal-map') || e.target.classList.contains('modal-map__script') || e.target.classList.contains('modal-map__close')) {
                 body.removeChild(map);
             }
         })
