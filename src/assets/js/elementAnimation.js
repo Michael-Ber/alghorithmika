f769cef5d02e569a6ccbb898b06c42e81a894e2a
unknown <mikeber000@gmail.com>
2024-05-03 19:59:32 +0800

    add animations

--- src/assets/js/elementAnimation.js
+++ src/assets/js/elementAnimation.js
@@ -3,8 +3,8 @@ export const elementAnimation = () => {
         const blocksToAnimate = document.querySelectorAll('[animate]');
         
         blocksToAnimate.forEach(block => {
-           
             const intersectionObserver = new IntersectionObserver((entries) => {
+                
                 if(entries[0].intersectionRatio <= 0) return;
                 
                 if(entries[0].target.hasAttribute('animate-y')) {
